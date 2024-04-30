c754a59bba749edabe47fd4dd4a24d0e485e79c0
NehaMadiwal <nehamadiwal@gmail.com>
2024-04-30 17:50:53 +0530

    Add app ui for trivia questions and progress (#1) (#2)
    * added UI for questions display
    
    * handled total number of questions
    
    * fixed formatting

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,9 +62,12 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
 
+    implementation(libs.androidx.viewmodel)
+
     //hilt-dagger
     implementation(libs.hilt)
     kapt(libs.hilt.compiler)
+    implementation(libs.hilt.navigation.compose)
 
     //coroutines
     implementation(libs.coroutine.core)
